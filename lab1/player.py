54fbce75b7e136683b8c75cd4d934b7ac0b7b1cc
Sujith <ps335@snu.edu.in>
2023-09-18 10:41:50 +0530

    complete

--- lab1/player.py
+++ lab1/player.py
@@ -1,34 +1,3 @@
-# The eight puzzle has 8 movable tiles within a 3 × 3 frame with one blank square. Imagine that in
-# each move the blank can be moved Up—Down—Left—Right. Note some moves may not be
-# possible in certain states. The goal of the puzzle is to reach from an initial configuration to a final
-# goal configuration that looks as follows: 1 2 3 4 5 6 7 8.
-# Clearly, the puzzle has 9! = 362880 states. It turns out that the goal state cannot be reached
-# from any arbitrary initial state. So, the set of states can be broken into two subsets a) states that
-# are reachable from the goal state and b) the rest that cannot be reached from the goal state. A
-# state can be represented as a string of length 9 where we write row1, row2, row3 as a sequence
-# with the blank symbolized by 0. So the goal state will be the string 012345678.
-
-
-# Based on the above description answer the questions below.
-# 1. Model the eight puzzle as a class named Puzzle using the above state representation. Define
-# methods that allow the puzzle to transition from state to state on a move. The move() method
-# can take a single integer argument that can have values 0, 1, 2, 3, that represents moving the
-# blank to the left, right, up, down respectively. If a particular move is impossible then it should
-# return the state None.
-
-
-# Also, implement a method named play() that takes a single positive integer as argument, say k,
-# and makes k moves starting with the current state of a puzzle object. Each move is chosen
-# randomly from the set of possible moves in the current state at each stage. [10,10,10=30]
-# 2. implement a) depth first search, b) breadth first search, c) depth limited search and d)
-# iterative deepening search. You can use default arguments in the generic graph method to pass
-# necessary arguments to reorder OPEN as needed for each type of search. [10,10,10,10=40]
-
-
-# 3. Use the four uninformed search algorithms in the previous question to search for a solution to
-# the 8-puzzle from a start state to a goal state. You can generate legal start states by using the
-# play() method with the initial state as the goal state. [10,10,10,10=40]
-
 import random
 
 class Puzzle:
@@ -76,25 +45,91 @@ class Puzzle:
     
     def breadth_first_search(self):
         visited = set()
-        queue = [(self, [])]
-        init_state = self.puzzle_string
+        queue = [(self, [], 0)]
         while queue:
-            current_puzzle, path = queue.pop(0)
-            visited.add(current_puzzle.puzzle_string)
-            if current_puzzle.is_solved():
-                path.insert(0, init_state)
+            (vertex, path, depth) = queue.pop(0)
+            if vertex.is_solved():
                 return path
-            for direction in range(4):
-                new_puzzle = Puzzle(current_puzzle.puzzle_string)
-                new_state = new_puzzle.move(direction)
-                if new_state is not None and new_state not in visited:
-                    new_path = path + [new_state]
-                    queue.append((new_puzzle, new_path))
+            if vertex not in visited:
+                visited.add(vertex)
+                for direction in range(4):
+                    new_puzzle = Puzzle(vertex.puzzle_string)
+                    if new_puzzle.move(direction) is not None:
+                        queue.append((new_puzzle, path + [direction], depth+1))
         return None
+    
+    def depth_first_search(self):
+        visited = set()
+        stack = [(self, [], 0)]
+        while stack:
+            (vertex, path, depth) = stack.pop()
+            if vertex.is_solved():
+                return path
+            if vertex not in visited:
+                visited.add(vertex)
+                for direction in range(4):
+                    new_puzzle = Puzzle(vertex.puzzle_string)
+                    if new_puzzle.move(direction) is not None:
+                        stack.append((new_puzzle, path + [direction], depth+1))
+                
+        return None
+    
+    def depth_limited_search(self, limit):
+        visited = set()
+        stack = [(self, [], 0,"")]
+        while stack:
+            (vertex, path, depth, state) = stack.pop()
+            if vertex.is_solved():
+                return path
+            if vertex.puzzle_string not in visited and depth < limit:
+                visited.add(vertex.puzzle_string)
+                for direction in range(4):
+                    new_puzzle = Puzzle(vertex.puzzle_string)
+                    if new_puzzle.move(direction) is not None:
+                        stack.append((new_puzzle, path + [direction], depth+1, state + vertex.puzzle_string))
+                
+        return None
+    
+    def iterative_deepening_search(self):
+        depth = 5
+        while True:
+            result = self.depth_limited_search(depth)
+            if result is not None:
+                return result
+            depth += 1
+            print("Depth: ", depth)
+    
+    def iterative_deepening_search(self,depth,increment):
+        while True:
+            result = self.depth_limited_search(depth)
+            if result is not None:
+                return result
+            depth += increment
+            print("Depth: ", depth)
+
+def print_path(path):
+    puzzle_path = []
+    if path is None:
+        print("No solution found")
+    else:
+        for direction in path:
+            if direction == 0:
+                puzzle_path.append("L")
+            elif direction == 1:
+                puzzle_path.append("R")
+            elif direction == 2:
+                puzzle_path.append("U")
+            elif direction == 3:
+                puzzle_path.append("D")
+        print(" -> ".join(puzzle_path))
+
 
 puzzle = Puzzle()
 print(puzzle.play(10))
-print(puzzle.breadth_first_search())
+print_path(puzzle.breadth_first_search())
+print_path(puzzle.depth_limited_search(10))
+print_path(puzzle.iterative_deepening_search())
+print_path(puzzle.depth_first_search())
 
     
 
