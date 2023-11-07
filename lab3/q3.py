42ce0a040980a00856705702307c930b6c5d200a
Sujith <ps335@snu.edu.in>
2023-11-07 22:26:15 +0530

    genetics

---
+++ lab3/q3.py
@@ -0,0 +1,14 @@
+# 3. Genetic Algorithms: 10*5=50
+# Problem Statement:
+# You are tasked with solving the Traveling Salesman Problem (TSP) using a genetic algorithm.
+# Given a set of cities and their pairwise distances, your goal is to find the shortest possible route
+# that visits each city exactly once and returns to the original city.
+# Requirements:
+# 1. Implement a genetic algorithm from scratch using a programming language of your choice.
+# 2. Use a suitable representation (e.g., permutation encoding) to represent candidate solutions.
+# 3. Define appropriate genetic operators (e.g., selection, crossover, mutation) for the TSP.
+# 4. Implement the fitness function to evaluate the quality of each candidate solution.
+# 5. Run the genetic algorithm for a specified number of generations and return the best solution
+# found.
+# Generate city maps of a few dozen cities and log the improvements over the generations
+
