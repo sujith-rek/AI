73636304c0a76a1e2c5fddbe65a99624807a3ae4
Sujith <ps335@snu.edu.in>
2023-11-07 22:25:54 +0530

    mcmc

---
+++ lab3/q1.py
@@ -0,0 +1,9 @@
+#  Markov regime-switching regression
+# 1. MCMC: 10+10
+# Download the monthly data for S&P 500 Index from Yahoo website from 2000 till date.
+# Perform Markov regime switching modelling and plot the regimes in the time series.
+# Print the transition probabilities.
+# Predict the future regimes for 5 time steps
+
+import yfinance as yf
+
