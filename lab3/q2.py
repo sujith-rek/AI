330a8f0d29a9e92c1f90a77517fa4c522a7bcb04
Sujith <ps335@snu.edu.in>
2023-11-07 22:26:05 +0530

    bayesian

---
+++ lab3/q2.py
@@ -0,0 +1,34 @@
+# 2. Bayesian Belief Networks: 10*4=40
+# Problem Statement:
+# You are given a dataset containing information about weather conditions (Temperature,
+# Humidity, Wind, and Outlook) and whether people played tennis or not. Your task is to
+# implement a Bayesian Belief Network (BBN) to predict whether people will play tennis based on
+# the given weather conditions.
+# Requirements:
+# 1. Implement a Bayesian Belief Network (BBN) from scratch using a programming language of
+# your choice (e.g., Python, Java, etc.).
+# 2. The BBN should consist of nodes representing the following variables: Temperature,
+# Humidity, Wind, Outlook, and PlayTennis.
+# 3. Use the dataset provided to learn the conditional probability tables (CPTs) for each node.
+# 4. Implement the inference algorithm (e.g., Variable Elimination) to make predictions about
+# whether people will play tennis given specific weather conditions.
+# Dataset:
+# | Outlook | Temperature | Humidity | Wind | PlayTennis |
+# |---------|-------------|----------|------|------------|
+# | Sunny | Hot | High | Weak | Yes |
+# | Sunny | Hot | High | Strong | No |
+# | Overcast | Hot | High | Weak | Yes |
+# | Rain | Mild | High | Weak | Yes |
+# | Rain | Cool | Normal | Weak | Yes |
+# | Rain | Cool | Normal | Strong | No |
+# | Overcast | Cool | Normal | Strong | Yes |
+# | Sunny | Mild | High | Weak | No |
+# | Sunny | Cool | Normal | Weak | Yes |
+# | Rain | Mild | Normal | Weak | Yes |
+# | Sunny | Mild | Normal | Strong | Yes |
+# | Overcast | Mild | High | Strong | Yes |
+# | Overcast | Hot | Normal | Weak | Yes |
+# | Rain | Mild | High | Strong | No |
+
+
+
