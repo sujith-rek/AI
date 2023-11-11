62f8fde4d7e74789aa2fcf86e467506af9d66116
Sujith <ps335@snu.edu.in>
2023-11-12 00:24:44 +0530

    update download

--- lab3/q1.py
+++ lab3/q1.py
@@ -1,9 +1,24 @@
 #  Markov regime-switching regression
 # 1. MCMC: 10+10
-# Download the monthly data for S&P 500 Index from Yahoo website from 2000 till date.
 # Perform Markov regime switching modelling and plot the regimes in the time series.
 # Print the transition probabilities.
 # Predict the future regimes for 5 time steps
 
-import yfinance as yf
+import pandas as pd
+import os
+
+# if file s&p500.csv is not present, download it from yahoo finance
+if not os.path.isfile('s&p500.csv'):
+    import yfinance as yf
+    data = yf.download('^GSPC', start="2000-01-01", end="2021-04-30")
+    data.to_csv('s&p500.csv')
+
+# read the data from s&p500.csv
+data = pd.read_csv('s&p500.csv')
+
+# Date,Open,High,Low,Close,Adj Close,Volume
+
+
+
+
 
